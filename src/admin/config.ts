ed3f0969b1b55919d3f7db94aae924e0043193f3
thinhpham67ag <thinhpham67ag@gmail.com>
2025-02-06 10:25:06 +0700

    update code

--- src/admin/config.ts
+++ src/admin/config.ts
@@ -1,7 +1,6 @@
-import 'dotenv/config'
 import session from 'express-session'
 import ConnectMongoDBSession from 'connect-mongodb-session'
-// import { Admin } from './models'
+import { Admin } from './models'
 
 const MongoDBStore = ConnectMongoDBSession(session);
 export const sessionStore = new MongoDBStore({
@@ -13,29 +12,29 @@ sessionStore.on('error', (error: string) => {
     console.log('Session store error', error)
 })
 
-// export const authenticate = async (email: string, password: string) => {
-//     if (email && password) {
-//         const user = await Admin.findOne({ email })
-//         if (!user) {
-//             return null
-//         }
-//         if (user.password === password) {
-//             return Promise.resolve({ email, password })
-//         } else {
-//             return null
-//         }
-//     }
-//     return null
-
-// }
-
 export const authenticate = async (email: string, password: string) => {
-    if (email === "thinhpham67ag@gmail.com" && password === "thinhpham031198") {
-        return Promise.resolve({ email, password })
-    } else {
-        return null
+    if (email && password) {
+        const user = await Admin.findOne({ email })
+        if (!user) {
+            return null
+        }
+        if (user.password === password) {
+            return Promise.resolve({ email, password })
+        } else {
+            return null
+        }
     }
+    return null
+
 }
+
+// export const authenticate = async (email: string, password: string) => {
+//     if (email === "thinhpham67ag@gmail.com" && password === "thinhpham031198") {
+//         return Promise.resolve({ email, password })
+//     } else {
+//         return null
+//     }
+// }
 
 
 export const { COOKIE_PASSWORD } = process.env
